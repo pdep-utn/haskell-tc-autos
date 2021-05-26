d8f171113d60fffd349ba6777d88947b860180da
Fernando Dodino <fernando.dodino@gmail.com>
2021-05-25 23:46:15 -0300

    Maybe => first alternative

--- src/Library.hs
+++ src/Library.hs
@@ -43,12 +43,22 @@ carreras = [
     rosario
     ]
 
--- -- Se pide que arme la tabla de posiciones indicando
--- -- Corredor, Puntos obtenidos
--- -- ordenándolo de mayor a menor
+
+-- *******************************************************************************************************
+-- Requerimiento
+--
+-- Se pide que arme la tabla de posiciones indicando (corredor, puntos obtenidos)
+-- ordenándolo de mayor a menor
+
+-- *******************************************************************************************************
+-- Tabla de posiciones - función general
+--
 tablaDePosiciones :: [Carrera] -> [Posicion]
 tablaDePosiciones carreras = (sortCorredores . map (\corredor -> (corredor, puntosObtenidos corredor carreras))) corredores
 
+-- *******************************************************************************************************
+-- Obtener los puntos de un corredor
+--
 puntosObtenidos :: Corredor -> [Carrera] -> Puntos
 puntosObtenidos corredor = foldr ((+) . puntosDeCorredor corredor) 0
 --puntosObtenidos corredor = foldl (\acum carrera -> acum + (findPuntos corredor) carrera) 0.0
@@ -59,13 +69,45 @@ corredor = fst
 puntos :: Posicion -> Puntos
 puntos = snd
 
+-- alternativa 1
+-- filtramos en la lista de posiciones aquellos que coinciden con el corredor, debería haber 0 ó 1
+-- si no hay ninguno (lista vacía) => lo asociamos con 0
+-- si hay alguno (debería ser 1 solo) => tomamos los puntos que es la segunda parte de la tupla
+
+-- puntosDeCorredor :: Corredor -> Carrera -> Puntos
+-- puntosDeCorredor unCorredor = puntosPorPosicion . filter ((== unCorredor) . corredor) . posiciones
+
+-- puntosPorPosicion :: [Posicion] -> Puntos
+-- puntosPorPosicion ((_, puntos):_) = puntos
+-- puntosPorPosicion []         = 0
+
+
+-- alternativa 2 heavy para calcular los puntos de un corredor en una carrera
+--
 puntosDeCorredor :: Corredor -> Carrera -> Puntos
-puntosDeCorredor unCorredor = puntosPorPosicion . filter ((== unCorredor) . corredor) . posiciones
+puntosDeCorredor unCorredor = puntosPorPosicion . safeHead . filter ((== unCorredor) . corredor) . posiciones
 
-puntosPorPosicion :: [Posicion] -> Puntos
-puntosPorPosicion ((_, puntos):_) = puntos
-puntosPorPosicion []         = 0
+-- el head no es seguro (safe), puede devolverte un valor o no, para eso existe el tipo de dato Maybe
+-- que se acompaña de otro tipo. En este caso si la lista es de aes, "maybe" te devuelvo un a, o "maybe not",
+-- en cuyo caso te devuelvo Nothing. El tipo de dato Maybe tiene dos constructores: Nothing cuando
+-- queremos representar el valor ausente, y Just (un valor) cuando el dato está presente.
+-- Un concepto similar son los optional de Java o los nullable (`?` de Typescript y Kotlin).
+--
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:xs) = Just x
 
+-- no sabemos si el corredor obtuvo puntos, entonces "maybe" (quizás) te paso los puntos
+-- o por ahí no te paso nada
+-- lo manejamos con pattern matching
+--
+puntosPorPosicion :: Maybe Posicion -> Number
+puntosPorPosicion Nothing       = 0
+puntosPorPosicion (Just (_, puntos)) = puntos
+
+-- *******************************************************************************************************
+-- Ordenamiento
+--
 sortCorredores :: [Posicion] -> [Posicion]
 sortCorredores = foldl ordenarCorredor []
 
@@ -75,26 +117,4 @@ ordenarCorredor (posicion1:posiciones) posicion
     | (puntos posicion > puntos posicion1) = (posicion:posicion1:posiciones)
     | otherwise                            = posicion1:ordenarCorredor posiciones posicion
 
-
--- alternativa heavy para calcular los puntos de un corredor en una carrera
---
--- findPuntosDeCorredor :: Corredor -> Carrera -> Number
--- findPuntosDeCorredor corredor = puntosPorPosicion . headPosible . filter (== corredor) . posiciones
-
--- el head no es seguro (safe), puede devolverte un valor o no, para eso existe el tipo de dato Maybe
--- que se acompaña de otro tipo. En este caso si la lista es de aes, "maybe" te devuelvo un a, o "maybe not",
--- en cuyo caso te devuelvo Nothing. El tipo de dato Maybe tiene dos constructores: Nothing cuando
--- queremos representar el valor ausente, y Just (un valor) cuando el dato está presente.
--- Un concepto similar son los optional de Java.
---
--- headPosible :: [a] -> Maybe a
--- headPosible [] = Nothing
--- headPosible (x:xs) = Just x
-
--- no sabemos si el corredor obtuvo puntos, entonces "maybe" (quizás) te paso los puntos
--- o por ahí no te paso nada
--- lo manejamos con pattern matching
---
--- puntosPorPosicion :: Maybe Number -> Number
--- puntosPorPosicion Nothing       = 0
--- puntosPorPosicion (Just puntos) = puntos
+-- *******************************************************************************************************

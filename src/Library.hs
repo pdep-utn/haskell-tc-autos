8be67fecbc451de6efa013194626c8458309e51b
Fernando Dodino <fernando.dodino@gmail.com>
2021-05-26 00:10:04 -0300

    quicksort + links

--- src/Library.hs
+++ src/Library.hs
@@ -108,13 +108,28 @@ puntosPorPosicion (Just (_, puntos)) = puntos
 -- *******************************************************************************************************
 -- Ordenamiento
 --
+-- alternativa 1 - por burbujeo
+-- https://www.youtube.com/watch?v=nmhjrI-aW5o
+--
+-- sortCorredores :: [Posicion] -> [Posicion]
+-- sortCorredores = foldl ordenarCorredor []
+
+-- ordenarCorredor :: [Posicion] -> Posicion -> [Posicion]
+-- ordenarCorredor [] posicion = [posicion]
+-- ordenarCorredor (posicion1:posiciones) posicion
+--     | (puntos posicion > puntos posicion1) = (posicion:posicion1:posiciones)
+--     | otherwise                            = posicion1:ordenarCorredor posiciones posicion
+
+
+-- alternativa 2
+-- usar quicksort
+-- https://www.youtube.com/watch?v=MZaf_9IZCrc
+-- 
 sortCorredores :: [Posicion] -> [Posicion]
-sortCorredores = foldl ordenarCorredor []
-
-ordenarCorredor :: [Posicion] -> Posicion -> [Posicion]
-ordenarCorredor [] posicion = [posicion]
-ordenarCorredor (posicion1:posiciones) posicion
-    | (puntos posicion > puntos posicion1) = (posicion:posicion1:posiciones)
-    | otherwise                            = posicion1:ordenarCorredor posiciones posicion
-
+sortCorredores []     = []
+sortCorredores (corredor:corredores) = (sortCorredores lesser) ++ [corredor] ++ (sortCorredores greater)
+    where
+        lesser  = filter (\(_, puntosOtro) -> puntosActual < puntosOtro) corredores
+        greater = filter (\(_, puntosOtro) -> puntosActual >= puntosOtro) corredores
+        puntosActual = puntos corredor
 -- *******************************************************************************************************
